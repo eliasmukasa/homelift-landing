30da8c4ae2dcf64a0333bca2e45d039916343f4a
Elias <eliasmukasa@gmail.com>
2025-06-14 14:55:59 -0400

    fix: correct component import path

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-// app/page.tsx
-import LandingPage from "@/components/LandingPage";
+import LandingPage from "../components/LandingPage";
 
 export default function Page() {
   return <LandingPage />;
